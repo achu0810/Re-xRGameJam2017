0963fb15c0e75109116e6610a2b6037ec0b60a2e
Yumuru <yakasa123@gmail.com>
2017-11-26 00:06:56 +0900

    取り敢えずButton追加

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+    [SerializeField]
+    Text text;
+    [SerializeField]
+    MeshRenderer show;
+
+    [SerializeField]
+    Texture selectTexture, pushedTexture;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void selected() {
+        show.material.mainTexture = selectTexture;
+    }
+
+    public void pushed() {
+        show.material.mainTexture = pushedTexture;
+    }
+}
