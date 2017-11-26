11e7667bfc9f659b90d0f0ca7c7640975c25a509
TeruyaHisashi <vxd.naoshi.19961205.maro@gmail.com>
2017-11-26 12:17:23 +0900

    EnemySystemの更新
    敵を生成するスクリプトの作成

---
+++ Assets/Script/EnemySystem/EnemyTower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTower : MonoBehaviour {
+
+	public List<GameObject> respawn;
+
+	public List<GameObject> Enemies;
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine(myUpdate());
+
+	}
+
+	// Update is called once per frame
+	IEnumerator myUpdate() {
+
+		while(true) {
+
+			yield return new WaitForSeconds(5f);
+
+
+			GameObject newEnemy = Instantiate(Enemies[Random.Range(0, Enemies.Count)]);
+			newEnemy.transform.position = respawn[Random.Range(0, respawn.Count)].transform.position;
+
+		}
+
+	}
+
+
+}
