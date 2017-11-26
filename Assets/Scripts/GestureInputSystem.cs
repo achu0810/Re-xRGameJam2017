727c01eed6808feff6758f05dcc9239ebbab3e51
Yumuru <yakasa123@gmail.com>
2017-11-26 11:27:39 +0900

    ButtonSystem追加

---
+++ Assets/Scripts/GestureInputSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.XR.WSA;
+using UnityEngine.XR.WSA.Persistence;
+using UnityEngine.XR.WSA.Input;
+
+public class GestureInputSystem : MonoBehaviour {
+
+   [SerializeField]
+   private GameObject ally;
+
+    public IConnectableObservable<Pair<RaycastHit>> lineOfSightObj;
+    public RaycastHit hit;
+
+    // Use this for initialization
+    void Start () {
+        InteractionManager.InteractionSourcePressed += SourcePressed;
+        lineOfSightObj = Service.RaycastSystem(transform, 10f)
+                                .Pairwise()
+                                .Publish();
+        lineOfSightObj.Subscribe(x => hit = x.Current).AddTo(gameObject);
+   }
+
+   // Update is called once per frame
+   void Update () {
+       RaycastHit hit;
+       if (Physics.Raycast(transform.position, transform.forward, out hit)){
+           hit.transform.GetComponent<Renderer>().material.color = new Color(255,0,0);
+       }
+   }
+
+   void SourcePressed(InteractionSourcePressedEventArgs state){
+       RaycastHit hit;
+       if(Physics.Raycast(transform.position, transform.forward, out hit, 8)){
+           Instantiate(ally,hit.transform.position,Quaternion.identity);
+       }
+   }
+
+}
---
+++ Assets/Scripts/Service.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public static class Service {
+    public static IObservable<RaycastHit> RaycastSystem(Transform transform, float length) {
+        return Observable.Create<RaycastHit>(observer => {
+            RaycastHit hit = new RaycastHit();
+            Observable.EveryUpdate()
+                .Select(_ => hit)
+                .Pairwise()
+                .Subscribe(h => {
+                    Physics.Raycast(transform.position, transform.forward, out hit, length);
+                    if (h.Previous.transform != hit.transform) {
+                        observer.OnNext(hit);
+                    }
+                }).AddTo(transform.gameObject);
+            return new CompositeDisposable();
+        });
+    }
+}
