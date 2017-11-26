04b67cea09717ff320b1d3fe6aeaabd6412bdda0
Achu <achu.zekamashi@gmail.com>
2017-11-26 14:24:41 +0900

    add Girl's status

---
+++ Assets/Scripts/AllyStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class AllyStatus : MonoBehaviour {
+
+	public float m_hp;
+	
+	void Start () {
+
+		this.ObserveEveryValueChanged (_ => m_hp)
+			.Where (x => x <= 0)
+			.Subscribe (_ => {
+				Destroy (this.gameObject);
+			});
+
+	}
+}
